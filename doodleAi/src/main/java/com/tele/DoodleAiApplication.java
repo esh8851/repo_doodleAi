0148c6408eb06ea62d6653ccc0ea20ad18ba061c
esh8851 <esh8851@gmail.com>
2024-11-08 15:58:09 +0900

    이선호

---
+++ doodleAi/src/main/java/com/tele/DoodleAiApplication.java
@@ -0,0 +1,13 @@
+package com.tele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodleAiApplication.class, args);
+	}
+
+}

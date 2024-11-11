7852c18ffda3dbf70eeffb24a517b58b16e9d1db
sinnosuek2701 <ehgml1111@naver.com>
2024-11-11 09:09:55 +0900

    이동현

--- doodleAi/src/main/java/com/tele/infra/act/ActService.java
+++ doodleAi/src/main/java/com/tele/infra/act/ActService.java
@@ -2,10 +2,12 @@ package com.tele.infra.act;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 @Service
 public class ActService {
 	
 	@Autowired
 	ActDao actDao;
+		
 	
 }
